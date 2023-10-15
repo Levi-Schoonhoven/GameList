ddf97a737fe8640bfdaebdc89dcf2a9f6c8642a0
Levi Schoonhoven <levi.schoonhoven@southeasttech.edu>
2023-10-15 13:17:45 -0500

    new controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,56 @@
+﻿using GameList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameList.Controllers
+{
+    public class GameController : Controller
+    {
+        private GameContext context { get; set; }
+        public GameController(GameContext ctx) {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Add() {
+            ViewBag.Action = "Add";
+            return View("Edit",new Game());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            ViewBag.Action = "Edit";
+            var game = context.Games.Find(id);
+            return View(game);
+        }
+        [HttpPost]
+        public IActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                if (game.GameId == 0)
+                    context.Games.Add(game);
+                else
+                    context.Games.Update(game);
+                context.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.Action = (game.GameId == 0) ? "Add" : "Edit";
+                return View(game);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var game = context.Games.Find(id);
+            return View(game);
+        }
+        [HttpPost]
+        public IActionResult Delete(Game game)
+        {
+            context.Games.Remove(game);
+            context.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
